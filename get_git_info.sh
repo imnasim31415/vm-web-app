ef8f6cd19b9b1590d6ece89badb1b6781934daf2
imnasim31415 <imnasim3.1415@gmail.com>
2025-06-12 20:42:03 +0600

    get_git_info.sh deleted

--- get_git_info.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
-HASH=$(git rev-parse HEAD)
-MESSAGE=$(git log -1 --pretty=%B)
-
-echo "$BRANCH" > git_info.txt
-echo "$HASH" >> git_info.txt
-echo "$MESSAGE" >> git_info.txt
